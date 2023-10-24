22b0e2ee4a11e1b6590ccb1e08cf757dc4bfd8e3
erika-lagos <144739255+erika-lagos@users.noreply.github.com>
2023-10-24 15:15:02 -0700

    Implement tab menu functionality
    Tab functionality is runnning correctly.
    Content for tabs is pending, excetp for Home tab.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,71 @@
 
 // import Background from './images/background.jpg';
 // Photo by <a href="https://unsplash.com/@toddquackenbush?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Todd Quackenbush</a> on <a href="https://unsplash.com/photos/black-mortar-and-pestle-beside-brown-box-in-top-view-photography-x5SRhkFajrA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-import loadContent from './pageLoad';
+import Logo from './images/logo-1.png';
+import loadHome from './loadHome';
 
 const mainContainer = document.querySelector('#content');
 
 function loadMainContent() {
-    mainContainer.innerHTML = '';
-    loadContent(mainContainer);
+    loadHeader(mainContainer);
+    loadHome(mainContainer);
+    toggleTab('Home');
 }
 
-// window.addEventListener('load', loadMainContent);
+function loadHeader(containerNode) {
+    const header = document.createElement('div');
+    header.className = 'header';
+    
+    const logo = new Image();
+    logo.src = Logo;
+    logo.className = 'logo';
+    header.appendChild(logo);
+    
+    const menuBar = document.createElement('div');
+    menuBar.className = 'menu-bar';
+    createMenuItems(menuBar, 'Home', 'Menu', 'Locations', 'Contact Us');
+    header.appendChild(menuBar);
+    
+    containerNode.appendChild(header);
+}
+
+function createMenuItems(menuBar, ...names) {
+    names.forEach(name => {
+        const item = document.createElement('div');
+        item.className = 'menu-item';
+        item.id = name.toLowerCase().replace(' ', '-');
+        item.textContent = name;
+        item.addEventListener('click', loadTab.bind(null, name));
+        menuBar.appendChild(item);
+    });
+}
+
+function toggleTab(tabName) {
+    const tabs = document.querySelectorAll('.menu-item');
+    tabs.forEach(tab => {
+        if (tab.textContent === tabName) {
+            tab.classList.add('selected');
+        } else {
+            tab.classList.remove('selected');
+        }
+    });
+}
+
+function loadTab(tabName) {
+    toggleTab(tabName);
+    const main = document.querySelector('.main');
+    mainContainer.removeChild(main);
+    if (tabName === 'Home') {
+        loadHome(mainContainer);
+    } else if (tabName === 'Menu') {
+        loadMenu(mainContainer);
+    } else if (tabName === 'Locations') {
+        loadLocations();
+    } else if (tabName === 'Contact Us') {
+        loadContact();
+    }
+}
+
+window.addEventListener('load', loadMainContent);
 
 
