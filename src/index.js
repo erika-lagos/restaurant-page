45ca617ac046fd1c5eea7744d944c31e975997e0
erika-lagos <144739255+erika-lagos@users.noreply.github.com>
2023-10-24 09:47:22 -0700

    Initial page layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+// import './style.css';
+// import Logo from './images/logo-2.png';
+// import Background from './images/background.jpg';
+// Photo by <a href="https://unsplash.com/@toddquackenbush?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Todd Quackenbush</a> on <a href="https://unsplash.com/photos/black-mortar-and-pestle-beside-brown-box-in-top-view-photography-x5SRhkFajrA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
 console.log('things are working');
+
+// const logo = new Image();
+// logo.src = Logo;
+  
